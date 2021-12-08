5063f6f15eb4dfc1fd137176c7f51dcfa6d78707
akoontz11 <austinkoontz11@gmail.com>
2021-12-08 11:11:10 -0600

    Calculated average retained reads

--- filtering_QC/analyzeReads.R
+++ filtering_QC/analyzeReads.R
@@ -1,6 +1,6 @@
-# %%%%%%%%%%%%%%%%%%%%%
-# %%% ANALYZE READS %%%
-# %%%%%%%%%%%%%%%%%%%%%
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%
+# %%% ANALYZE READ COUNTS %%%
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%
 # Extract total and per sample read values from file
 # This file was generated using stacks-dist-extract process_radtags.log per_barcode_raw_read_counts
 
@@ -9,20 +9,25 @@ totalReads <- (read.table("metric-retainedReads_allsamples", header=TRUE)[,2])
 
 # Extract 3rd column: retained reads per sample
 retainedReads <- (read.table("metric-retainedReads_allsamples", header=TRUE)[,3])
-
 # Calculate each sample's retained read proportion
 retainedSampleProportion <- (retainedReads/totalReads)*100
-# Data exploration
-print(c("Minimum retained read proportion", min(retainedSampleProportion), "Maximum retained read proportion", max(retainedSampleProportion)))
 
 # Calculate the number of retained reads in entire dataset
 allReads <- sum(retainedReads)
-
 # Calculate each sample's contribution to the entire dataset
 totalSampleProportion <- (retainedReads/allReads)*100
-# Data exploration
-print(c("Minimum read contribution", min(totalSampleProportion), "Maximum read contribution", max(totalSampleProportion)))
 
 # Combine vectors
 read.mat <- cbind(read.table("metric-retainedReads_allsamples", header=TRUE)[,1:3], 
                   retainedSampleProportion, totalSampleProportion)
+
+# Data exploration
+# Retained reads
+mean(retainedReads); sd(retainedReads)
+read.mat[which.min(read.mat[,3]),]; read.mat[which.max(read.mat[,3]),]
+# Retained read proportion: mean, standard deviation, min/max
+mean(retainedSampleProportion); sd(retainedSampleProportion)
+read.mat[which.min(read.mat[,4]),]; read.mat[which.max(read.mat[,4]),]
+# Total sample proportion: mean, standard deviation, min/max
+mean(totalSampleProportion); sd(totalSampleProportion)
+read.mat[which.min(read.mat[,5]),]; read.mat[which.max(read.mat[,5]),]
