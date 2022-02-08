1c5dd1d87efd2705662970ee91baea9c74e93001
akoontz11 <austinkoontz11@gmail.com>
2022-02-08 13:06:04 -0600

    Altered preliminary subset code for NJT of samples

---
+++ denovoAssembly/finalAssemblies/QUBO/popAnalysis/dendrogram_plot.R
@@ -0,0 +1,34 @@
+# Plot unrooted Neighbor-Joining Tree
+
+library(ape)
+library(hierfstat)
+library(adegenet)
+
+# Update graphing parameters (margins), to make trees more legible in Google doc
+# Default
+d_par.mai <- par("mai")
+par(mai=c(0.25,0.25,0.25,0.25)) 
+
+# "Whitelist" loci (1,000 random loci)
+# Pull in genepop object (with file suffix updated--Stacks writes as ".genepop", but needs to be ".gen")
+prelimWL_genind <- read.genepop("populations.snps.gen")
+# Number of loci
+length(prelimWL_genind@loc.n.all)
+# Create distance matrix, based off of SNP matrix (individuals in rows, alleles in columns)
+D_WL <- dist(tab(prelimWL_genind))
+# Create Neighbor-Joining Tree, based off of SNP matrix
+tree_WL <- nj(D_WL)
+plot(tree_WL, type="unrooted", edge.w=2, main="Preliminary Subset: 1,000 loci")
+plot(tree_WL, type="phylogram", edge.w=2, main="1,000 loci")
+
+# All loci
+# Pull in genepop object (with file suffix updated--Stacks writes as ".genepop", but needs to be ".gen")
+prelimAll_genind <- read.genepop("populations.snps.gen")
+# Number of loci
+length(prelimAll_genind@loc.n.all)
+# Create distance matrix, based off of SNP matrix (individuals in rows, alleles in columns)
+D <- dist(tab(prelimAll_genind))
+# Create Neighbor-Joining Tree, based off of SNP matrix
+tree <- nj(D)
+plot(tree, type="unrooted", edge.w=2, main="Preliminary Subset: 47,266 loci")
+plot(tree, type="phylogram", edge.w=2, main="47,266 loci")
