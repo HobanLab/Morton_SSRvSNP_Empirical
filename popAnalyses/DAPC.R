98c6cb4a177528d32f69116e1397a07ab819ab33
akoontz11 <austinkoontz11@gmail.com>
2022-08-05 15:51:22 -0500

    Latest run; cbind groupings for garden AND wild samples

--- popAnalyses/DAPC.R
+++ popAnalyses/DAPC.R
@@ -10,6 +10,9 @@
 library(adegenet)
 library(scales)
 
+# TO DO: Emily wants you to run a normal PCA on the dataset (i.e. not through adegenet; instructions
+# in adegenet Basics tutorial)
+
 # %%%% QUAC %%%% ----
 # Specify filepath for finalized QUAC dataset, with both garden and wild samples, and the following filters:
 # R80 (no filter for missing data), no minor allele frequency filter, first SNP per locus, 2 populations specified (garden and wild)
@@ -21,15 +24,15 @@ QUAC.genind <- read.genepop(paste0(QUAC.genpop.filepath,"populations.snps.gen"))
 # Use k-means clustering to find a number of groups which maximizes the variation between groups
 # (after transforming the data using PCA, in order to increase computation times)
 QUAC.grp <- find.clusters(QUAC.genind, max.n.clust=20)
-# Number of retained PCs (here, there is no cost to retaining a lot of PCs): 200
-# Number of clusters (seeking to minimize the Bayesian Information Criterion value): 4
+# Retained PCs (here, there is no cost to retaining a lot of PCs): 200
+# Clusters (seeking to minimize the Bayesian Information Criterion value): 4
 QUAC.grp
 
 # Conduct DAPC: transform the data using PCA, then run discriminant analysis on retained principal components,
 # using the inferred groupings from the k-means clustering step above
 QUAC.dapc1 <- dapc(x=QUAC.genind, pop=QUAC.grp$grp)
-# Number of retained PCs (specifying a large value can lead to overfitting, and unstable membership probability): 50
-# Number of discriminant functions to retain (for less than 10 clusters, all eigenvalues can be retained): 4
+# Retained PCs (specifying a large value can lead to overfitting/unstable membership probability): 50
+# Discriminant functions to retain (for less than 10 clusters, all eigenvalues can be retained): 2
 
 # List grouping of each individual (helpful for determining correct coloration)
 QUAC.dapc1$grp
@@ -41,8 +44,8 @@ cbind(QUAC.dapc1$grp[97:193], read.table("/RAID1/IMLS_GCCO/Analysis/Stacks/denov
 scatter(QUAC.dapc1, scree.da=F, bg="white", pch=20, cell=0, cstar=0, solid=0.6, clab=0, legend=T,
         posi.leg=locator(n=1), cleg=1.0, cex=2, inset.solid=1,
         col = c("#66A61E","#E7298A","#7570B3","#2171B5"),
-        txt.leg = c("Porter/Kessler/Sugarloaf/Magazine Mt. 1","Pryor Mt.","Magazine Mt. 2"))
-mtext("QUAC R80: Garden and Wild", adj=0.25)
+        txt.leg = c("Pryor Mt.","Magazine Mt. 2", "Porter Mt.", "Kessler/Sugarloaf/Magazine Mt. 1"))
+mtext("QUAC R80: Garden and Wild", adj=0.02)
 
 # %%%% QUBO %%%% ----
 # Specify filepath for finalized QUBO dataset, with both garden and wild samples, and the following filters:
@@ -55,15 +58,15 @@ QUBO.genind <- read.genepop(paste0(QUBO.genpop.filepath,"populations.snps.gen"))
 # Use k-means clustering to find a number of groups which maximizes the variation between groups
 # (after transforming the data using PCA, in order to increase computation times)
 QUBO.grp <- find.clusters(QUBO.genind, max.n.clust=20)
-# Number of retained PCs (here, there is no cost to retaining a lot of PCs): 200
-# Number of clusters (seeking to minimize the Bayesian Information Criterion value): 2
+# Retained PCs (here, there is no cost to retaining a lot of PCs): 200
+# Clusters (seeking to minimize the Bayesian Information Criterion value): 3
 QUBO.grp
 
 # Conduct DAPC: transform the data using PCA, then run discriminant analysis on retained principal components,
 # using the inferred groupings from the k-means clustering step above
 QUBO.dapc1 <- dapc(x=QUBO.genind, pop=QUBO.grp$grp)
-# Number of retained PCs (specifying a large value can lead to overfitting, and unstable membership probability): 50
-# Number of discriminant functions to retain (for less than 10 clusters, all eigenvalues can be retained): 4
+# Retained PCs (specifying a large value can lead to overfitting, and unstable membership probability): 50
+# Discriminant functions to retain (for less than 10 clusters, all eigenvalues can be retained): 4
 
 # List grouping of each individual (helpful for determining correct coloration)
 QUBO.dapc1$grp
@@ -71,9 +74,17 @@ QUBO.dapc1$grp[86:180]
 read.table("/RAID1/IMLS_GCCO/Analysis/Stacks/reference_filteredReads/QUBO/QUBO_popmap_wild")[,2]
 cbind(QUBO.dapc1$grp[86:180], read.table("/RAID1/IMLS_GCCO/Analysis/Stacks/reference_filteredReads/QUBO/QUBO_popmap_wild")[,2])
 
+QUBO.dapc1$grp
+read.table("/RAID1/IMLS_GCCO/Analysis/Stacks/reference_filteredReads/QUBO/QUBO_popmap_GardenWild")[1:85,2]
+c(read.table("/RAID1/IMLS_GCCO/Analysis/Stacks/reference_filteredReads/QUBO/QUBO_popmap_GardenWild")[1:85,2],
+  read.table("/RAID1/IMLS_GCCO/Analysis/Stacks/reference_filteredReads/QUBO/QUBO_popmap_wild")[,2])
+cbind(QUBO.dapc1$grp, c(read.table("/RAID1/IMLS_GCCO/Analysis/Stacks/reference_filteredReads/QUBO/QUBO_popmap_GardenWild")[1:85,2],
+                        read.table("/RAID1/IMLS_GCCO/Analysis/Stacks/reference_filteredReads/QUBO/QUBO_popmap_wild")[,2]))
+
 # Plot DAPC as a scatterplot
 scatter(QUBO.dapc1, scree.da=F, bg="white", pch=20, cell=0, cstar=0, solid=0.6, clab=0, legend=T,
         posi.leg=locator(n=1), cleg=1.0, cex=2, inset.solid=1,
         col = c("#66A61E","#E7298A","#7570B3","#2171B5"), 
-        txt.leg = c("EBSCO/Blue Trail Kings Chair/Peavine/Wattsville","Pop11/Irondale","Moss Rock/Oakbrook"))
-mtext("QUBO R80: Garden and Wild", adj=1)
+        txt.leg = c("Moss Rock/Oakbrook","EBSCO/Blue Trail Kings Chair/Peavine/Wattsville",
+                    "Hinds Road/Pop 11/Wattsville"))
+mtext("QUBO R80: Garden and Wild", adj=0.9)
