e01bf1e6a225033a33838e8c802c28030f1d5b7c
akoontz11 <austinkoontz11@gmail.com>
2022-10-14 11:39:42 -0500

    Updated comments

--- radseqAnalyses/filtering_QC/fastQC/FastQC_Run.sh
+++ radseqAnalyses/filtering_QC/fastQC/FastQC_Run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# The below commands are used to extract data from compressed (*.gz) files and pass it to fastqc, which will generate a report of data quality
-#Each of these commands are sent within a directory containing the sample files.
+# FastQC checks for the expected number of reads, read length, overall quality, and duplicate rate
+# Using the CLI, FastQC can parse fastq.gz files. We'll get aggregate scores of all the sequences passed to the program
+# Run this command in the directory containing the raw data (the .fastq.gz files)
+# zcat on its own prints the output of unzipping these files to screen; the pipe directs the print output to fastqc, so files remain compressed
+# Wildcards (?) are to prevent inclusion of sampples with failed barcodes (named "Undetermined.fastq.gz"); 8 is the number of cores specified
 
 # For processing raw, demultiplexed files (prior to running process_radtags)
 zcat ????????-????????_*.fastq.gz | fastqc stdin:fastqc_results -t 8
