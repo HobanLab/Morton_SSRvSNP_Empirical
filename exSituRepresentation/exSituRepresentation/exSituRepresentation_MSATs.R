b6f742c8941e515f0080bc865dbe25ca0eb3eea8
akoontz11 <austinkoontz11@gmail.com>
2023-02-01 17:25:11 -0600

    Updated QUAC analyses, to refer to woK/NoK MSAT/SNP objects; removed extraneous set.wd commands

--- exSituRepresentation/exSituRepresentation/exSituRepresentation_MSATs.R
+++ exSituRepresentation/exSituRepresentation/exSituRepresentation_MSATs.R
@@ -3,13 +3,15 @@
 # %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 # This script uses the functions declared in the functions_exSituRepresentation.R file to generate 
-# ex situ representation values for Quercus acerifolia (QUAC) and Quercus boyntonii (QUBO)
+# ex situ representation values for Quercus acerifolia (QUAC) and Quercus boyntonii (QUBO). QUAC 
+# datasets without Kessler individuals are analyzed.
 
 # For both species, it reads in datasets from optimized de novo assemblies built using Stacks 
 # (QUAC: m 7, M/n 5, gt-alpha 0.01; QUBO: m 7, M/n 5, gt-alpha 0.01)
 # and datasets from reference alignments ("GSNAP4" alignment parameters; QUAC: Q. rubra genome;
-# QUBO: Q. robur genome). Finally, for both de novo and refernce datasets, it reads in loci
-# unfiltered according to missing data (R0) and loci shared among at least 80% (R80) of all samples (garden and wild)
+# QUBO: Q. robur genome). Finally, for both de novo and reference datasets, it reads in loci
+# unfiltered according to missing data (R0) and loci shared among at least 80% (R80) of all samples 
+# (garden and wild)
 
 # It does this for both the SNP (NextRAD) and MSAT datasets, using both complete datasets
 # and partial datasets (samples subset to only those included within both studies)
@@ -18,27 +20,26 @@ library(adegenet)
 
 # %%%% FUNCTIONS %%%% ----
 # Read in relevant functions required for ex situ representation analyses
-SSRvSNP.wd <- "~/Documents/SSRvSNP/Code/"
+SSRvSNP.wd <- "/home/akoontz/Documents/SSRvSNP/Code/"
 setwd(SSRvSNP.wd)
 source("exSituRepresentation/functions_exSituRepresentation.R")
 
 # %%%% QUAC %%%% ----
 # ---- MSATS: COMPLETE ----
 QUAC.MSAT.filePath <- 
-  "~/Documents/peripheralProjects/GCC_QUAC_ZAIN/Data_Files/"
-setwd(QUAC.MSAT.filePath)
+  "/home/akoontz/Documents/peripheralProjects/GCC_QUAC_ZAIN/Data_Files/"
 QUAC.MSAT.genind <- 
-  read.genepop(paste0(QUAC.MSAT.filePath, "Adegenet_Files/QUAC_wK_allpop_clean.gen"), ncode = 3)
+  read.genepop(paste0(QUAC.MSAT.filePath, "Adegenet_Files/QUAC_woK_allpop_clean.gen"), ncode = 3)
 # Assign sample names: read in Tissue database names from GCC_QUAC_ZAIN repository
 QUAC.MSAT.tissueNames <- 
-  unlist(read.csv2(paste0(QUAC.MSAT.filePath, "Data_Frames/QUAC_allpop_clean_df.csv"), header = TRUE, sep=",")[1])
+  unlist(read.csv2(paste0(QUAC.MSAT.filePath, "Data_Frames/QUAC_woK_allpop_clean_df.csv"), header = TRUE, sep=",")[1])
 rownames(QUAC.MSAT.genind@tab) <- QUAC.MSAT.tissueNames
 # Correct popNames: samples with popName pattern QAc-G- are garden 
-levels(QUAC.MSAT_genind@pop)[grep(pattern = "QAc-G-", levels(QUAC.MSAT_genind@pop))] <- 
-  rep("garden", length(grep(pattern = "QAc-G-", levels(QUAC.MSAT_genind@pop))))
+levels(QUAC.MSAT.genind@pop)[grep(pattern = "QAc-G-", levels(QUAC.MSAT.genind@pop))] <- 
+  rep("garden", length(grep(pattern = "QAc-G-", levels(QUAC.MSAT.genind@pop))))
 # Correct popNames: samples with popName pattern QAc-W- are wild
-levels(QUAC.MSAT_genind@pop)[grep(pattern = "QAc-W-", levels(QUAC.MSAT_genind@pop))] <- 
-  rep("wild", length(grep(pattern = "QAc-W-", levels(QUAC.MSAT_genind@pop))))
+levels(QUAC.MSAT.genind@pop)[grep(pattern = "QAc-W-", levels(QUAC.MSAT.genind@pop))] <- 
+  rep("wild", length(grep(pattern = "QAc-W-", levels(QUAC.MSAT.genind@pop))))
 # Report representation of wild alleles in gardens
 exSitu_Rep(QUAC.MSAT.genind)
 # Exploration of total and wild allele frequency proportions
@@ -47,13 +48,13 @@ getTotalAlleleFreqProportions(QUAC.MSAT.genind)
 
 # ---- SNPS, DE NOVO: COMPLETE ----
 # R0 ----
-# Read in genind file: Optimized de novo assembly; R0, min-maf=0, first SNP/locus, 2 populations (garden and wild)
+# Read in genind file: Optimized de novo assembly; R0, min-maf=0, first SNP/locus, 2 populations (garden and wild), no Kessler individuals
 genpop.filePath <- 
-  "/RAID1/IMLS_GCCO/Analysis/Stacks/denovo_finalAssemblies/QUAC/output/populations_R0_NOMAF_1SNP_2Pops/"
-setwd(genpop.filePath)
+  "/RAID1/IMLS_GCCO/Analysis/Stacks/denovo_finalAssemblies/QUAC/output/populations_R0_NOMAF_1SNP_2Pops_NoK/"
 QUAC.SNP.DN.R0.genind <- read.genepop(paste0(genpop.filePath,"populations.snps.gen"))
 # Correct popNames
-pop(QUAC.SNP.DN.R0.genind) <- factor(read.table("QUAC_popmap_GardenWild", header=FALSE)[,2])
+pop(QUAC.SNP.DN.R0.genind) <- 
+  factor(read.table(paste0(genpop.filePath, "QUAC_popmap_GardenWild_NoK"), header=FALSE)[,2])
 # Report representation of wild alleles in gardens
 exSitu_Rep(QUAC.SNP.DN.R0.genind)
 # Exploration of total and wild allele frequency proportions
@@ -61,13 +62,13 @@ getWildAlleleFreqProportions(QUAC.SNP.DN.R0.genind)
 getTotalAlleleFreqProportions(QUAC.SNP.DN.R0.genind)
 
 # R80 ----
-# Read in genind file: Optimized de novo assembly; R80, min-maf=0, first SNP/locus, 2 populations (garden and wild)
+# Read in genind file: Optimized de novo assembly; R80, min-maf=0, first SNP/locus, 2 populations (garden and wild), no Kessler individuals
 genpop.filePath <- 
-  "/RAID1/IMLS_GCCO/Analysis/Stacks/denovo_finalAssemblies/QUAC/output/populations_R80_NOMAF_1SNP_2Pops/"
-setwd(genpop.filePath)
+  "/RAID1/IMLS_GCCO/Analysis/Stacks/denovo_finalAssemblies/QUAC/output/populations_R80_NOMAF_1SNP_2Pops_NoK/"
 QUAC.SNP.DN.R80.genind <- read.genepop(paste0(genpop.filePath,"populations.snps.gen"))
 # Correct popNames
-pop(QUAC.SNP.DN.R80.genind) <- factor(read.table("QUAC_popmap_GardenWild", header=FALSE)[,2])
+pop(QUAC.SNP.DN.R80.genind) <- 
+  factor(read.table(paste0(genpop.filePath, "QUAC_popmap_GardenWild_NoK"), header=FALSE)[,2])
 # Report representation of wild alleles in gardens
 exSitu_Rep(QUAC.SNP.DN.R80.genind)
 # Exploration of total and wild allele frequency proportions
@@ -76,13 +77,13 @@ getTotalAlleleFreqProportions(QUAC.SNP.DN.R80.genind)
 
 # ---- SNPS, REFERENCE: COMPLETE ----
 # R0 ----
-# Read in genind file: QUAC GSNAP4 alignment; R0, min-maf=0, first SNP/locus, 2 populations (garden and wild)
+# Read in genind file: QUAC GSNAP4 alignment; R0, min-maf=0, first SNP/locus, 2 populations (garden and wild), no Kessler individuals
 genpop.filePath <- 
-  "/RAID1/IMLS_GCCO/Analysis/Stacks/reference_filteredReads/QUAC/Q_rubra/output/populations_R0_NOMAF_1SNP_2Pops/"
-setwd(genpop.filePath)
+  "/RAID1/IMLS_GCCO/Analysis/Stacks/reference_filteredReads/QUAC/Q_rubra/output/populations_R0_NOMAF_1SNP_2Pops_NoK/"
 QUAC.SNP.REF.R0.genind <- read.genepop(paste0(genpop.filePath,"populations.snps.gen"))
 # Correct popNames
-pop(QUAC.SNP.REF.R0.genind) <- factor(read.table("QUAC_popmap_GardenWild", header=FALSE)[,2])
+pop(QUAC.SNP.REF.R0.genind) <- 
+  factor(read.table(paste0(genpop.filePath, "QUAC_popmap_GardenWild_NoK"), header=FALSE)[,2])
 # Representation rates
 exSitu_Rep(QUAC.SNP.REF.R0.genind)
 # Exploration of total and wild allele frequency proportions
@@ -90,13 +91,13 @@ getWildAlleleFreqProportions(QUAC.SNP.REF.R0.genind)
 getTotalAlleleFreqProportions(QUAC.SNP.REF.R0.genind)
 
 # R80 ----
-# Read in genind file: QUAC GSNAP4 alignment; R80, min-maf=0, first SNP/locus, 2 populations (garden and wild)
+# Read in genind file: QUAC GSNAP4 alignment; R80, min-maf=0, first SNP/locus, 2 populations (garden and wild), no Kessler individuals
 genpop.filePath <- 
-  "/RAID1/IMLS_GCCO/Analysis/Stacks/reference_filteredReads/QUAC/Q_rubra/output/populations_R80_NOMAF_1SNP_2Pops/"
-setwd(genpop.filePath)
+  "/RAID1/IMLS_GCCO/Analysis/Stacks/reference_filteredReads/QUAC/Q_rubra/output/populations_R80_NOMAF_1SNP_2Pops_NoK/"
 QUAC.SNP.REF.R80.genind <- read.genepop(paste0(genpop.filePath,"populations.snps.gen"))
 # Correct popNames
-pop(QUAC.SNP.REF.R80.genind) <- factor(read.table("QUAC_popmap_GardenWild", header=FALSE)[,2])
+pop(QUAC.SNP.REF.R80.genind) <- 
+  factor(read.table(paste0(genpop.filePath,"QUAC_popmap_GardenWild_NoK"), header=FALSE)[,2])
 # Representation rates
 exSitu_Rep(QUAC.SNP.REF.R80.genind)
 # Exploration of total and wild allele frequency proportions
@@ -106,13 +107,14 @@ getTotalAlleleFreqProportions(QUAC.SNP.REF.R80.genind)
 # ---- MSATS AND SNPS: SUBSET ----
 # MSAT: read in Tissue database names from GCC_QUAC_ZAIN repository, and rename MSAT genind matrix
 QUAC.MSAT.tissueNames_filepath <- 
-  "~/Documents/peripheralProjects/GCC_QUAC_ZAIN/Data_Files/Data_Frames/QUAC_allpop_clean_df.csv"
+  "/home/akoontz/Documents/peripheralProjects/GCC_QUAC_ZAIN/Data_Files/Data_Frames/QUAC_woK_allpop_clean_df.csv"
 QUAC.MSAT.tissueNames <- unlist(read.csv2(QUAC.MSAT.tissueNames_filepath, header = TRUE, sep=",")[1])
 rownames(QUAC.MSAT.genind@tab) <- QUAC.MSAT.tissueNames
 
 # SNP: read in Tissue database names, and rename SNP genind matrix
-# This file was created (by Austin K.), and can be found on the Hoban Lab Drive ("MSATcomparisons_TissueNames")
-QUAC.SNP.tissueNames_filepath <- paste0(SSRvSNP.wd,"exSituRepresentation/Resampling/QUAC_SNP_TissueNames.csv")
+# Austin Koontz created this file, and it lives on the Hoban Lab Drive 
+# ("MSATcomparisons_TissueNames:QUAC_NextRAD_TissueDatabaseNames")
+QUAC.SNP.tissueNames_filepath <- paste0(SSRvSNP.wd,"exSituRepresentation/QUAC_SNP_TissueNames.csv")
 QUAC.SNP.tissueNames <- unlist(read.csv2(QUAC.SNP.tissueNames_filepath, header = TRUE, sep = ",")[3])
 rownames(QUAC.SNP.DN.R0.genind@tab) <- rownames(QUAC.SNP.REF.R0.genind@tab) <- QUAC.SNP.tissueNames
 rownames(QUAC.SNP.DN.R80.genind@tab) <- rownames(QUAC.SNP.REF.R80.genind@tab) <- QUAC.SNP.tissueNames
@@ -159,8 +161,7 @@ getTotalAlleleFreqProportions(QUAC.SNP.REF.R80_subset.genind)
 # ---- MSATS: COMPLETE ----
 # Read in genind file (Southeast Oaks repo; genetic_data/Qb_total.gen file)
 genpop.filePath <- 
-  "~/Documents/peripheralProjects/SE_oaks_genetics/genetic_data/"
-setwd(genpop.filePath)
+  "/home/akoontz/Documents/peripheralProjects/SE_oaks_genetics/genetic_data/"
 QUBO.MSAT.genind <- read.genepop(paste0(genpop.filePath,"Qb_total.gen"), ncode=3)
 # Correct popNames: last population (IMLS4_MP1_IMLS336_C05) is garden; rest (9) are wild
 levels(QUBO.MSAT.genind@pop) <- c(rep("wild",9), "garden") 
@@ -175,10 +176,10 @@ getTotalAlleleFreqProportions(QUBO.MSAT.genind)
 # Read in genind file: Optimized de novo assembly; R0, min-maf=0, first SNP/locus, 2 populations (garden and wild)
 genpop.filePath <- 
   "/RAID1/IMLS_GCCO/Analysis/Stacks/denovo_finalAssemblies/QUBO/output/populations_R0_NOMAF_1SNP_2Pops/"
-setwd(genpop.filePath)
 QUBO.SNP.DN.R0.genind <- read.genepop(paste0(genpop.filePath,"populations.snps.gen"))
 # Correct popNames
-pop(QUBO.SNP.DN.R0.genind) <- factor(read.table("QUBO_popmap_GardenWild", header=FALSE)[,2])
+pop(QUBO.SNP.DN.R0.genind) <- 
+  factor(read.table(paste0(genpop.filePath,"QUBO_popmap_GardenWild"), header=FALSE)[,2])
 # Report representation of wild alleles in gardens
 exSitu_Rep(QUBO.SNP.DN.R0.genind)
 # Exploration of total and wild allele frequency proportions
@@ -192,7 +193,8 @@ genpop.filePath <-
 setwd(genpop.filePath)
 QUBO.SNP.DN.R80.genind <- read.genepop(paste0(genpop.filePath,"populations.snps.gen"))
 # Correct popNames
-pop(QUBO.SNP.DN.R80.genind) <- factor(read.table("QUBO_popmap_GardenWild", header=FALSE)[,2])
+pop(QUBO.SNP.DN.R80.genind) <- 
+  factor(read.table(paste0(genpop.filePath,"QUBO_popmap_GardenWild"), header=FALSE)[,2])
 # Report representation of wild alleles in gardens
 exSitu_Rep(QUBO.SNP.DN.R80.genind)
 # Exploration of total and wild allele frequency proportions
@@ -204,10 +206,10 @@ getTotalAlleleFreqProportions(QUBO.SNP.DN.R80.genind)
 # Read in genind file: QUBO GSNAP4 alignment; R0, min-maf=0, first SNP/locus, 2 populations (garden and wild)
 genpop.filePath <- 
   "/RAID1/IMLS_GCCO/Analysis/Stacks/reference_filteredReads/QUBO/GSNAP4/output/populations_R0_NOMAF_1SNP_2Pops/"
-setwd(genpop.filePath)
 QUBO.SNP.REF.R0.genind <- read.genepop(paste0(genpop.filePath,"populations.snps.gen"))
 # Correct popNames
-pop(QUBO.SNP.REF.R0.genind) <- factor(read.table("QUBO_popmap_GardenWild", header=FALSE)[,2])
+pop(QUBO.SNP.REF.R0.genind) <- 
+  factor(read.table(paste0(genpop.filePath,"QUBO_popmap_GardenWild"), header=FALSE)[,2])
 # Report representation of wild alleles in gardens
 exSitu_Rep(QUBO.SNP.REF.R0.genind)
 # Exploration of total and wild allele frequency proportions
@@ -218,10 +220,10 @@ getTotalAlleleFreqProportions(QUBO.SNP.REF.R0.genind)
 # Read in genind file: QUBO GSNAP4 alignment; R80, min-maf=0, first SNP/locus, 2 populations (garden and wild)
 genpop.filePath <- 
   "/RAID1/IMLS_GCCO/Analysis/Stacks/reference_filteredReads/QUBO/GSNAP4/output/populations_R80_NOMAF_1SNP_2Pops/"
-setwd(genpop.filePath)
 QUBO.SNP.REF.R80.genind <- read.genepop(paste0(genpop.filePath,"populations.snps.gen"))
 # Correct popNames
-pop(QUBO.SNP.REF.R80.genind) <- factor(read.table("QUBO_popmap_GardenWild", header=FALSE)[,2])
+pop(QUBO.SNP.REF.R80.genind) <- 
+  factor(read.table(paste0(genpop.filePath,"QUBO_popmap_GardenWild"), header=FALSE)[,2])
 # Report representation of wild alleles in gardens
 exSitu_Rep(QUBO.SNP.REF.R80.genind)
 # Exploration of total and wild allele frequency proportions
