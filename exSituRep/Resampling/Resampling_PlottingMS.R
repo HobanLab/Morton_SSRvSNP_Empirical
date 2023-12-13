d194ea102508cd3698ee26db17764fb596574fa0
akoontz11 <austinkoontz11@gmail.com>
2023-12-13 17:26:49 -0600

    Correcting figure numbers

--- exSituRep/Resampling/Resampling_PlottingMS.R
+++ exSituRep/Resampling/Resampling_PlottingMS.R
@@ -24,7 +24,7 @@ source("exSituRep/functions_exSituRepresentation.R")
 # Specify path to the directory (on the lab server), where resampling arrays are held
 resamplingDataDir <- paste0(SSRvSNP.wd, "exSituRep/Resampling/resamplingData/")
 # Specify path to the directory (on the lab server), where resampling plots (PDFs) will be saved
-imageOutDir <- "/home/akoontz/Documents/SSRvSNP/Documentation/Images/MolEcol_202305_Images/"
+imageOutDir <- "/home/akoontz/Documents/SSRvSNP/Documentation/Images/EvolApp_2023_Images/FINAL/"
 # Plotting colors (for all plots!)
 plotColors <- c("red","red4","darkorange3","coral","purple")
 
@@ -54,19 +54,16 @@ QUAC.SNP.REF.R80.Subset.min95_mean <- resample_min95_mean(QUAC.SNP.REF.R80.Subse
 
 # PLOTTING ----
 # Call pdf command, to save resampling plots to disk
-pdf(file = paste0(imageOutDir, "mainText/Fig_2.pdf"), width = 9, height = 7.5)
+pdf(file = paste0(imageOutDir, "mainText/Fig_1.pdf"), width = 9, height = 7.5)
 # Set plotting window to stack 3 graphs vertically
 # Margin values are changed prior to each plot, in order to allow for sufficient margins at the top/bottom
 par(mfcol=c(3,1), oma=rep(0.2,4), mar=c(2,4.5,4,1.5)+0.1)
-# Create two vectors for colors. This is to show points on the graph and in the legend clearly
-fullColors <- plotColors
-fadedColors <- c(plotColors[1], alpha(plotColors[2:5], 0.5))
 # MSAT
 # Plots all sets of points onto single graph, as well as 95% threshold line
-matplot(QUAC.MSAT.Subset.meanValuesMat, ylim=c(0,110), col=fadedColors, pch=16, ylab="")
+matplot(QUAC.MSAT.Subset.meanValuesMat, ylim=c(0,110), col=plotColors, pch=16, ylab="")
 title("Microsatellites", line=0.15, cex=2)
 legend(x=75, y=76, inset = 0.05, legend = c("Total","Very common","Common","Low frequency", "Rare"),
-       col=fullColors, pch = c(20,20,20), cex=1.2, pt.cex = 2, bty="n", y.intersp = 1.3)
+       col=plotColors, pch = c(20,20,20), cex=1.2, pt.cex = 2, bty="n", y.intersp = 1.3)
 # Lines for 95% threshold
 abline(h=95, col="black", lty=3); abline(v=QUAC.MSAT.Subset.min95_mean, col="black")
 # Text for number of individuals to capture 95% threshold, with position based on variable
@@ -78,10 +75,10 @@ mtext(text="QUAC: Subset Datasets (91 Samples)",
 # SNP DN R80
 par(mar=c(3,4.5,3,1.5)+0.1)
 # Plots all sets of points onto single graph, as well as 95% threshold line
-matplot(QUAC.SNP.DN.R80.Subset.meanValuesMat, ylim=c(0,110), col=fadedColors, pch=16, ylab="")
+matplot(QUAC.SNP.DN.R80.Subset.meanValuesMat, ylim=c(0,110), col=plotColors, pch=16, ylab="")
 title("SNPs: De novo (R80)", line=0.15, cex=2)
 legend(x=75, y=76, inset = 0.05, legend = c("Total","Very common","Common","Low frequency", "Rare"),
-       col=fullColors, pch = c(20,20,20), cex=1.2, pt.cex = 2, bty="n", y.intersp = 1.3)
+       col=plotColors, pch = c(20,20,20), cex=1.2, pt.cex = 2, bty="n", y.intersp = 1.3)
 # Lines for 95% threshold
 abline(h=95, col="black", lty=3); abline(v=QUAC.SNP.DN.R80.Subset.min95_mean, col="black")
 # Text for number of individuals to capture 95% threshold, with position based on variable
@@ -92,10 +89,10 @@ mtext(text="Wild allelic diversity (%)", side=2, line=3, cex=1.2, srt=90)
 # SNP REF R80
 par(mar=c(4,4.5,2,1.5)+0.1)
 # Plots all sets of points onto single graph, as well as 95% threshold line
-matplot(QUAC.SNP.REF.R80.Subset.meanValuesMat, ylim=c(0,110), col=fadedColors, pch=16, ylab="")
+matplot(QUAC.SNP.REF.R80.Subset.meanValuesMat, ylim=c(0,110), col=plotColors, pch=16, ylab="")
 title("SNPs: Reference (R80)", line=0.15, cex=2)
 legend(x=75, y=76, inset = 0.05, legend = c("Total","Very common","Common","Low frequency", "Rare"),
-       col=fullColors, pch = c(20,20,20), cex=1.2, pt.cex = 2, bty="n", y.intersp = 1.3)
+       col=plotColors, pch = c(20,20,20), cex=1.2, pt.cex = 2, bty="n", y.intersp = 1.3)
 # Lines for 95% threshold
 abline(h=95, col="black", lty=3); abline(v=QUAC.SNP.REF.R80.Subset.min95_mean, col="black")
 # Text for number of individuals to capture 95% threshold, with position based on variable
@@ -129,19 +126,16 @@ QUBO.SNP.REF.R80.Subset.min95_mean <- resample_min95_mean(QUBO.SNP.REF.R80.Subse
 
 # PLOTTING ----
 # Call pdf command, to save resampling plots to disk
-pdf(file = paste0(imageOutDir, "mainText/Fig_3.pdf"), width = 9, height = 7.5)
+pdf(file = paste0(imageOutDir, "mainText/Fig_2.pdf"), width = 9, height = 7.5)
 # Set plotting window to stack 3 graphs vertically
 # Margin values are changed prior to each plot, in order to allow for sufficient margins at the top/bottom
 par(mfcol=c(3,1), oma=rep(0.2,4), mar=c(2,4.5,4,1.5)+0.1)
-# Create two vectors for colors. This is to show points on the graph and in the legend clearly
-fullColors <- plotColors
-fadedColors <- c(plotColors[1], alpha(plotColors[2:5], 0.5))
 # MSAT
 # Plots all sets of points onto single graph, as well as 95% threshold line
-matplot(QUBO.MSAT.Subset.meanValuesMat, ylim=c(0,110), col=fadedColors, pch=16, ylab="")
+matplot(QUBO.MSAT.Subset.meanValuesMat, ylim=c(0,110), col=plotColors, pch=16, ylab="")
 title("Microsatellites", line=0.15, cex=2)
 legend(x=80, y=76, inset = 0.05, legend = c("Total","Very common","Common","Low frequency", "Rare"),
-       col=fullColors, pch = c(20,20,20), cex=1.2, pt.cex = 2, bty="n", y.intersp = 1.3)
+       col=plotColors, pch = c(20,20,20), cex=1.2, pt.cex = 2, bty="n", y.intersp = 1.3)
 # Lines for 95% threshold
 abline(h=95, col="black", lty=3); abline(v=QUBO.MSAT.Subset.min95_mean, col="black")
 # Text for number of individuals to capture 95% threshold, with position based on variable
@@ -153,10 +147,10 @@ mtext(text="QUBO: Subset Datasets (94 Samples)",
 # SNP DN R80
 par(mar=c(3,4.5,3,1.5)+0.1)
 # Plots all sets of points onto single graph, as well as 95% threshold line
-matplot(QUBO.SNP.DN.R80.Subset.meanValuesMat, ylim=c(0,110), col=fadedColors, pch=16, ylab="")
+matplot(QUBO.SNP.DN.R80.Subset.meanValuesMat, ylim=c(0,110), col=plotColors, pch=16, ylab="")
 title("SNPs: De novo (R80)", line=0.15, cex=2)
 legend(x=80, y=76, inset = 0.05, legend = c("Total","Very common","Common","Low frequency", "Rare"),
-       col=fullColors, pch = c(20,20,20), cex=1.2, pt.cex = 2, bty="n", y.intersp = 1.3)
+       col=plotColors, pch = c(20,20,20), cex=1.2, pt.cex = 2, bty="n", y.intersp = 1.3)
 # Lines for 95% threshold
 abline(h=95, col="black", lty=3); abline(v=QUBO.SNP.DN.R80.Subset.min95_mean, col="black")
 # Text for number of individuals to capture 95% threshold, with position based on variable
@@ -167,10 +161,10 @@ mtext(text="Wild allelic diversity (%)", side=2, line=3, cex=1.2, srt=90)
 # SNP REF R80
 par(mar=c(4,4.5,2,1.5)+0.1)
 # Plots all sets of points onto single graph, as well as 95% threshold line
-matplot(QUBO.SNP.REF.R80.Subset.meanValuesMat, ylim=c(0,110), col=fadedColors, pch=16, ylab="")
+matplot(QUBO.SNP.REF.R80.Subset.meanValuesMat, ylim=c(0,110), col=plotColors, pch=16, ylab="")
 title("SNPs: Reference (R80)", line=0.15, cex=2)
 legend(x=80, y=76, inset = 0.05, legend = c("Total","Very common","Common","Low frequency", "Rare"),
-       col=fullColors, pch = c(20,20,20), cex=1.2, pt.cex = 2, bty="n", y.intersp = 1.3)
+       col=plotColors, pch = c(20,20,20), cex=1.2, pt.cex = 2, bty="n", y.intersp = 1.3)
 # Lines for 95% threshold
 abline(h=95, col="black", lty=3); abline(v=QUBO.SNP.REF.R80.Subset.min95_mean, col="black")
 # Text for number of individuals to capture 95% threshold, with position based on variable
@@ -189,14 +183,14 @@ dev.off()
 # MSAT ----
 # Complete
 QUAC.MSAT.Complete.arrayPath <- paste0(resamplingDataDir,"QUAC.MSAT.Complete_resampArr.Rdata")
-QUAC.MSAT.Complete.imagePath <- paste0(imageOutDir,"supplement/Fig_S8.pdf")
+QUAC.MSAT.Complete.imagePath <- paste0(imageOutDir,"supplement/Fig_S10.pdf")
 # Generate resampling plot
 resample_singlePlot_PDF(QUAC.MSAT.Complete.arrayPath, QUAC.MSAT.Complete.imagePath,  
                   colors = plotColors, xLeg=120, yLeg=70, minSampleLineDist=40, 
                   mainText = "QUAC, Microsatellites (Complete: 164 samples)")
 # Subset
 QUAC.MSAT.Subset.arrayPath <- paste0(resamplingDataDir,"QUAC.MSAT.Subset_resampArr.Rdata")
-QUAC.MSAT.Subset.imagePath <- paste0(imageOutDir,"supplement/Fig_S9.pdf")
+QUAC.MSAT.Subset.imagePath <- paste0(imageOutDir,"supplement/Fig_S11.pdf")
 # Generate resampling plot
 resample_singlePlot_PDF(QUAC.MSAT.Subset.arrayPath, QUAC.MSAT.Subset.imagePath,  
                   colors = plotColors, xLeg=65, yLeg=70, minSampleLineDist=25, 
@@ -206,7 +200,7 @@ resample_singlePlot_PDF(QUAC.MSAT.Subset.arrayPath, QUAC.MSAT.Subset.imagePath,
 # Complete
 QUAC.SNP.DN.R0.Complete.arrayPath <- paste0(resamplingDataDir,"QUAC.SNP.DN.R0.Complete_resampArr.Rdata")
 QUAC.SNP.DN.R80.Complete.arrayPath <- paste0(resamplingDataDir,"QUAC.SNP.DN.R80.Complete_resampArr.Rdata")
-QUAC.SNP.DN.Complete.imagePath <- paste0(imageOutDir,"supplement/Fig_S10.pdf")
+QUAC.SNP.DN.Complete.imagePath <- paste0(imageOutDir,"supplement/Fig_S12.pdf")
 # Generate resampling plot (R0 and R80)
 resample_doublePlot_PDF(QUAC.SNP.DN.R0.Complete.arrayPath, QUAC.SNP.DN.R80.Complete.arrayPath, 
                         QUAC.SNP.DN.Complete.imagePath, colors = plotColors, xLeg=76, yLeg=70, minSampleLineDist=20,
@@ -215,7 +209,7 @@ resample_doublePlot_PDF(QUAC.SNP.DN.R0.Complete.arrayPath, QUAC.SNP.DN.R80.Compl
 # Subset
 QUAC.SNP.DN.R0.Subset.arrayPath <- paste0(resamplingDataDir,"QUAC.SNP.DN.R0.Subset_resampArr.Rdata")
 QUAC.SNP.DN.R80.Subset.arrayPath <- paste0(resamplingDataDir,"QUAC.SNP.DN.R80.Subset_resampArr.Rdata")
-QUAC.SNP.DN.Subset.imagePath <- paste0(imageOutDir,"supplement/Fig_S11.pdf")
+QUAC.SNP.DN.Subset.imagePath <- paste0(imageOutDir,"supplement/Fig_S13.pdf")
 # Generate resampling plot (R0 and R80)
 resample_doublePlot_PDF(QUAC.SNP.DN.R0.Subset.arrayPath, QUAC.SNP.DN.R80.Subset.arrayPath, 
                         QUAC.SNP.DN.Subset.imagePath, colors = plotColors, xLeg=76, yLeg=70, minSampleLineDist=20,
@@ -225,7 +219,7 @@ resample_doublePlot_PDF(QUAC.SNP.DN.R0.Subset.arrayPath, QUAC.SNP.DN.R80.Subset.
 # Complete
 QUAC.SNP.REF.R0.Complete.arrayPath <- paste0(resamplingDataDir,"QUAC.SNP.REF.R0.Complete_resampArr.Rdata")
 QUAC.SNP.REF.R80.Complete.arrayPath <- paste0(resamplingDataDir,"QUAC.SNP.REF.R80.Complete_resampArr.Rdata")
-QUAC.SNP.REF.Complete.imagePath <- paste0(imageOutDir,"supplement/Fig_S12.pdf")
+QUAC.SNP.REF.Complete.imagePath <- paste0(imageOutDir,"supplement/Fig_S14.pdf")
 # Generate resampling plot (R0 and R80)
 resample_doublePlot_PDF(QUAC.SNP.REF.R0.Complete.arrayPath, QUAC.SNP.REF.R80.Complete.arrayPath, 
                         QUAC.SNP.REF.Complete.imagePath, colors = plotColors, xLeg=76, yLeg=70, minSampleLineDist=20,
@@ -234,7 +228,7 @@ resample_doublePlot_PDF(QUAC.SNP.REF.R0.Complete.arrayPath, QUAC.SNP.REF.R80.Com
 # Subset
 QUAC.SNP.REF.R0.Subset.arrayPath <- paste0(resamplingDataDir,"QUAC.SNP.REF.R0.Subset_resampArr.Rdata")
 QUAC.SNP.REF.R80.Subset.arrayPath <- paste0(resamplingDataDir,"QUAC.SNP.REF.R80.Subset_resampArr.Rdata")
-QUAC.SNP.REF.Subset.imagePath <- paste0(imageOutDir,"supplement/Fig_S13.pdf")
+QUAC.SNP.REF.Subset.imagePath <- paste0(imageOutDir,"supplement/Fig_S15.pdf")
 # Generate resampling plot (R0 and R80)
 resample_doublePlot_PDF(QUAC.SNP.REF.R0.Subset.arrayPath, QUAC.SNP.REF.R80.Subset.arrayPath, 
                         QUAC.SNP.REF.Subset.imagePath, colors = plotColors, xLeg=76, yLeg=70, minSampleLineDist=20,
@@ -244,14 +238,14 @@ resample_doublePlot_PDF(QUAC.SNP.REF.R0.Subset.arrayPath, QUAC.SNP.REF.R80.Subse
 # MSAT ----
 # Complete
 QUBO.MSAT.Complete.arrayPath <- paste0(resamplingDataDir,"QUBO.MSAT.Complete_resampArr.Rdata")
-QUBO.MSAT.Complete.imagePath <- paste0(imageOutDir,"supplement/Fig_S14.pdf")
+QUBO.MSAT.Complete.imagePath <- paste0(imageOutDir,"supplement/Fig_S16.pdf")
 # Generate resampling plot
 resample_singlePlot_PDF(QUBO.MSAT.Complete.arrayPath, QUBO.MSAT.Complete.imagePath,  
                         colors = plotColors, xLeg=190, yLeg=70, minSampleLineDist=60, 
                         mainText = "QUBO, Microsatellites (Complete: 245 samples)")
 # Subset
 QUBO.MSAT.Subset.arrayPath <- paste0(resamplingDataDir,"QUBO.MSAT.Subset_resampArr.Rdata")
-QUBO.MSAT.Subset.imagePath <- paste0(imageOutDir,"supplement/Fig_S15.pdf")
+QUBO.MSAT.Subset.imagePath <- paste0(imageOutDir,"supplement/Fig_S17.pdf")
 # Generate resampling plot
 resample_singlePlot_PDF(QUBO.MSAT.Subset.arrayPath, QUBO.MSAT.Subset.imagePath,  
                         colors = plotColors, xLeg=78, yLeg=40, minSampleLineDist=25, 
@@ -261,7 +255,7 @@ resample_singlePlot_PDF(QUBO.MSAT.Subset.arrayPath, QUBO.MSAT.Subset.imagePath,
 # Complete
 QUBO.SNP.DN.R0.Complete.arrayPath <- paste0(resamplingDataDir,"QUBO.SNP.DN.R0.Complete_resampArr.Rdata")
 QUBO.SNP.DN.R80.Complete.arrayPath <- paste0(resamplingDataDir,"QUBO.SNP.DN.R80.Complete_resampArr.Rdata")
-QUBO.SNP.DN.Complete.imagePath <- paste0(imageOutDir,"supplement/Fig_S16.pdf")
+QUBO.SNP.DN.Complete.imagePath <- paste0(imageOutDir,"supplement/Fig_S18.pdf")
 # Generate resampling plot (R0 and R80)
 resample_doublePlot_PDF(QUBO.SNP.DN.R0.Complete.arrayPath, QUBO.SNP.DN.R80.Complete.arrayPath, 
                         QUBO.SNP.DN.Complete.imagePath, colors = plotColors, xLeg=80, yLeg=65, minSampleLineDist=25,
@@ -270,7 +264,7 @@ resample_doublePlot_PDF(QUBO.SNP.DN.R0.Complete.arrayPath, QUBO.SNP.DN.R80.Compl
 # Subset
 QUBO.SNP.DN.R0.Subset.arrayPath <- paste0(resamplingDataDir,"QUBO.SNP.DN.R0.Subset_resampArr.Rdata")
 QUBO.SNP.DN.R80.Subset.arrayPath <- paste0(resamplingDataDir,"QUBO.SNP.DN.R80.Subset_resampArr.Rdata")
-QUBO.SNP.DN.Subset.imagePath <- paste0(imageOutDir,"supplement/Fig_S17.pdf")
+QUBO.SNP.DN.Subset.imagePath <- paste0(imageOutDir,"supplement/Fig_S19.pdf")
 # Generate resampling plot (R0 and R80)
 resample_doublePlot_PDF(QUBO.SNP.DN.R0.Subset.arrayPath, QUBO.SNP.DN.R80.Subset.arrayPath, 
                         QUBO.SNP.DN.Subset.imagePath, colors = plotColors, xLeg=80, yLeg=65, minSampleLineDist=25,
@@ -280,7 +274,7 @@ resample_doublePlot_PDF(QUBO.SNP.DN.R0.Subset.arrayPath, QUBO.SNP.DN.R80.Subset.
 # Complete
 QUBO.SNP.REF.R0.Complete.arrayPath <- paste0(resamplingDataDir,"QUBO.SNP.REF.R0.Complete_resampArr.Rdata")
 QUBO.SNP.REF.R80.Complete.arrayPath <- paste0(resamplingDataDir,"QUBO.SNP.REF.R80.Complete_resampArr.Rdata")
-QUBO.SNP.REF.Complete.imagePath <- paste0(imageOutDir,"supplement/Fig_S18.pdf")
+QUBO.SNP.REF.Complete.imagePath <- paste0(imageOutDir,"supplement/Fig_S20.pdf")
 # Generate resampling plot (R0 and R80)
 resample_doublePlot_PDF(QUBO.SNP.REF.R0.Complete.arrayPath, QUBO.SNP.REF.R80.Complete.arrayPath, 
                         QUBO.SNP.REF.Complete.imagePath, colors = plotColors, xLeg=80, yLeg=65, minSampleLineDist=25,
@@ -289,7 +283,7 @@ resample_doublePlot_PDF(QUBO.SNP.REF.R0.Complete.arrayPath, QUBO.SNP.REF.R80.Com
 # Subset
 QUBO.SNP.REF.R0.Subset.arrayPath <- paste0(resamplingDataDir,"QUBO.SNP.REF.R0.Subset_resampArr.Rdata")
 QUBO.SNP.REF.R80.Subset.arrayPath <- paste0(resamplingDataDir,"QUBO.SNP.REF.R80.Subset_resampArr.Rdata")
-QUBO.SNP.REF.Subset.imagePath <- paste0(imageOutDir,"supplement/Fig_S19.pdf")
+QUBO.SNP.REF.Subset.imagePath <- paste0(imageOutDir,"supplement/Fig_S21.pdf")
 # Generate resampling plot (R0 and R80)
 resample_doublePlot_PDF(QUBO.SNP.REF.R0.Subset.arrayPath, QUBO.SNP.REF.R80.Subset.arrayPath, 
                         QUBO.SNP.REF.Subset.imagePath, colors = plotColors, xLeg=80, yLeg=65, minSampleLineDist=25,
